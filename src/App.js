b1aba22861bd0485c4326185cc085272b4f09664
Cody Samuels <twowoodenspoons@gmail.com>
2020-08-07 18:30:25 -0700

    Denis' starter code completed.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import React, { Component } from 'react';
 import axios from "axios"
 import './App.css';
 
+
+function EmployeeCard({ img, name, phone }) {
+
+  return (
+    <>
+      <div>
+        <img src={img} alt={name.first} />
+        <div>
+          <p>{`${name.title} ${name.first} ${name.last}`}</p>
+          <p>{phone}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+const styles={
+  employeeContainer:{
+    display:"flex",
+    flexWrap: "wrap",
+    jsutifyContent: "center"
+  }
+}
+
 class App extends Component {
 
   state = {
     numInput: 0,
-    users:[]
+    users: []
   }
 
   handleInputChange = e => {
@@ -16,19 +40,25 @@ class App extends Component {
 
   makeRequest = async () => {
     const URL = `https://randomuser.me/api/?results=${this.state.numInput}&nat=us`
-    let data;
-    try {
-      data = await axios.get(URL)
-      let {
-        data: { results },
-      } = data;
 
-      this.setState({users:results})
-      
+    try {
+      let results = await axios.get(URL)
+
+      this.setState({ users: results.data.results })
+
       console.log(results)
     } catch (e) {
       console.log("Error:", e)
     }
+  }
+
+  renderEmployees = () => {
+    return this.state.users.map(user => <EmployeeCard
+      key={user.id.value}
+      img={user.picture.large}
+      name={user.name}
+      phone={user.phone}
+    />);
   }
 
   render() {
@@ -50,8 +80,8 @@ class App extends Component {
         </label>
         <button disabled={isNumberEntered} onClick={this.makeRequest} className="btn btn-primary">{isNumberEntered ? "Please Enter a Number" : "Submit"}</button>
 
-        <div className="container employeeContainer">
-          {this.state.numInput}
+        <div style={styles.employeeContainer}>
+          {this.renderEmployees()}
         </div>
       </div>
     );
