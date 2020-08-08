bd555ebf98d7ed4094086c51018647b69884001b
Cody Samuels <twowoodenspoons@gmail.com>
2020-08-07 17:36:48 -0700

    added axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from "axios"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    numInput: 0,
+    users:[]
+  }
+
+  handleInputChange = e => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+
+  makeRequest = async () => {
+    const URL = `https://randomuser.me/api/?results=${this.state.numInput}&nat=us`
+    let data;
+    try {
+      data = await axios.get(URL)
+      let {
+        data: { results },
+      } = data;
+
+      this.setState({users:results})
+      
+      console.log(results)
+    } catch (e) {
+      console.log("Error:", e)
+    }
+  }
+
+  render() {
+
+    const isNumberEntered = this.state.numInput === 0
+
+    return (
+      <div className="App">
+        <h1>Sabre International</h1>
+        <label htmlFor="numInput"># of Employees
+      <input
+            id="numInput"
+            name="numInput"
+            type="number"
+            value={this.state.numInput}
+            min="0"
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <button disabled={isNumberEntered} onClick={this.makeRequest} className="btn btn-primary">{isNumberEntered ? "Please Enter a Number" : "Submit"}</button>
+
+        <div className="container employeeContainer">
+          {this.state.numInput}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
