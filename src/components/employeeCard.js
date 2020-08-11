73a07d525e14f2e7a3b46565f8130cf93ab0dfb0
Cody Samuels <twowoodenspoons@gmail.com>
2020-08-10 18:28:01 -0700

    rounded images

--- src/components/employeeCard.js
+++ src/components/employeeCard.js
@@ -6,7 +6,7 @@ function EmployeeCard({ img, name, phone, email }) {
     return (
       <>
         <div style={{margin:"2rem"}}>
-          <img className="card-img-top" style={{padding:"5rem", paddingBottom:"0px"}} src={img} alt={name.first} />
+          <img className="rounded-circle" src={img} alt={name.first} />
           <div className="card-body">
             <h5 className="card-title">{`${name.first} ${name.last}`}</h5>
             <p className="card-text">{phone}</p>
